07670ce37c50512edaea9c792e3fde1a5a23b710
Jay Kakkad <jkakkad33@gmail.com>
2024-08-10 16:16:51 +0530

    Fixed responsiveness in about section

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,17 @@ import HeroSection from "@/components/HeroSection";
 import MyWorkSection from "@/components/MyWorkSection";
 import NavBar from "@/components/NavBar";
 import QuoteComponent from "@/components/QuoteComponent";
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({
+  subsets:['latin'],
+  weight:['400','700'],
+})
+
 
 export default function Home() {
   return (
-    <div className="px-6 md:px-3 bg-gradient-to-br from-black via-purple-950 to-pink-950 scroll-smooth transition-all duration-1000">
+    <div className={`px-6 md:px-3 bg-gradient-to-br from-black via-purple-950 to-pink-950 scroll-smooth transition-all duration-1000 ${raleway.className}`}>
       <NavBar/>
       <HeroSection/>
       <MyWorkSection/>
--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,38 +5,38 @@ import Link from 'next/link'
 
 const AboutSection = () => {
   return (
-    <section className='pt-24 p-5 lg:px-40 lg:py-20 min-h-screen flex flex-col lg:flex-row gap-5' id='about'>
+    <section className='pt-24 p-5 lg:px-40 min-h-screen flex flex-col lg:flex-row gap-5 lg:py-20 w-full' id='about'>
         {/* Flex container to ensure both sides have equal height */}
-        <div className='flex flex-1 gap-5'>
-            <WhiteBlock
-                elements={
-                    <Image 
+        <div className='flex flex-col lg:flex-row gap-5'>
+            <div className='w-full h-full lg:w-[35%]'>
+                <WhiteBlock
+                    elements={
+                        <Image 
                         src='/images/person.png' 
                         width={1000} 
-                        height={1000} 
-                        alt='My Image' 
-                        className='object-cover w-full h-full'
-                    />
-                }
-                css='w-[35%] h-full'
-            />
-
-            <div className='flex flex-col flex-1 gap-5'>
+                        height={1000}
+                        className='md:h-full sm:w-full sm:h-full scale-[1.1]'
+                        />
+                    }
+                    css='w-full h-full overflow-hidden'
+                />
+            </div>
+            <div className='flex flex-col gap-5 w-full lg:w-[65%]'>
                 <WhiteBlock 
                     elements={
-                        <div className='flex justify-between items-center gap-5'>
+                        <div className='flex justify-between items-center'>
                             <div>
-                                <h1 className='text-3xl sm:text-4xl lg:mb-4 lg:text-8xl'>About Me</h1>
+                                <h1 className='text-3xl sm:text-4xl lg:mb-4 lg:text-7xl'>About Me</h1>
                                 <span className='text-sm lg:text-4xl mt-5'>UIUX/Graphics Designer</span>
                             </div>
-                            <Image src='/images/03 shaka.png' width={10} height={10} className='scale-x-[-1] w-20 h-20 lg:w-40 lg:h-40'/>
+                            <Image src='/images/03 shaka.png' width={1000} height={1000} className='scale-x-[-1] w-20 h-20 lg:w-40 lg:h-40'/>
                         </div>
                     }
                     css='p-5 lg:px-5 lg:py-5 w-full'
                 />
                 <WhiteBlock 
                     elements={
-                        <p className='text-sm lg:text-xl'>Hi, My name is Prashant. I am a passionate designer filled with creative juices and a designer bookworm. Master of graphics and intern of UIUX.</p>
+                        <p className='text-sm md:text-xl'>Hi, My name is Prashant. I am a passionate designer filled with creative juices and a designer bookworm. Master of graphics and intern of UIUX.</p>
                     }
                     css='px-5 lg:px-8 pb-10 lg:pb-20 pt-4'
                 />
@@ -58,13 +58,14 @@ const AboutSection = () => {
                                 </div>
                             </div>
                         }
-                        css='p-8 w-full'
+                        css='p-8 lg:w-auto w-full'
                     />
                     <WhiteBlock 
                         elements={
-                            <Link href='/' className='text-xs lg:text-xs underline'>Know more</Link>
+                            <Link href='/' className='text-xl lg:text-2xl underline'>Know more</Link>
                         }
-                        css=' rounded-2xl lg:px-12 lg:py-10'
+                        // css=' rounded-2xl lg:px-12 lg:py-10'
+                        css='p-5 lg:px-12 lg:py-10 text-center'
                     />
                 </div>
             </div>
